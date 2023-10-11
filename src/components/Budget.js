2d70e56a6aa87d3ba31cafb91c32a92d5972a77e
faszari50 <fasihahabdrazak@gmail.com>
2023-10-11 17:33:56 +0800

    dadd

--- src/components/Budget.js
+++ src/components/Budget.js
@@ -2,15 +2,29 @@ import React, { useContext, useState } from 'react';
 import { AppContext } from '../context/AppContext';
 
 const Budget = () => {
-    const { budget } = useContext(AppContext);
+    const { budget, remaining, totalExpenses } = useContext(AppContext);
     const [newBudget, setNewBudget] = useState(budget);
+
     const handleBudgetChange = (event) => {
-        setNewBudget(event.target.value);
+        const newInput = event.target.value;
+
+        if(newInput > 2000){
+            alert("The value cannot exceed remaining fund £"+remaining);
+            setNewBudget('');
+        }
+        else if(newInput < totalExpenses){
+            alert("You cannot reduce the budget value lower than than the spending £"+totalExpenses);
+            setNewBudget('');
+        }
+        else{
+            setNewBudget(newInput);
+        }
     }
+ 
     return (
 <div className='alert alert-secondary'>
 <span>Budget: £{budget}</span>
-<input type="number" step="10" value={newBudget} onChange={handleBudgetChange}></input>
+<input type="number" step="10" max="20000" value={newBudget} onChange={handleBudgetChange}></input>
 </div>
     );
 };
--- src/components/ExpenseTotal.js
+++ src/components/ExpenseTotal.js
@@ -1,10 +1,12 @@
 import React, { useContext } from 'react';
 import { AppContext } from '../context/AppContext';
 const ExpenseTotal = () => {
-    const { expenses } = useContext(AppContext);
+    const {totalExpenses} = useContext(AppContext);
+    
+    /*const { expenses } = useContext(AppContext);
     const totalExpenses = expenses.reduce((total, item) => {
         return (total += item.cost);
-    }, 0);
+    }, 0); */
     return (
         <div className='alert alert-primary'>
             <span>Spent so far: £{totalExpenses}</span>
